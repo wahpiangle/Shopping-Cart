c2df729f058246d4ef42b72043c739d90c5ee720
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-03-31 00:14:07 +0800

    update stripe functionality

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,23 +5,32 @@ require('dotenv').config()
 const stripe = require('stripe')(process.env.VITE_STRIPE_KEY);
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 
 const YOUR_DOMAIN = 'https://vercel.com/wahpiangle/shopping-cart';
 
 app.post('/create-checkout-session', async (req, res) => {
+    const cartTotalPrice = req.body;
     const session = await stripe.checkout.sessions.create({
-        //TODO change this to price only
-        // line_items: [
-        //     {
-        //         price: '{{PRICE_ID}}',
-        //         quantity: 1,
-        //     },
-        // ],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: 'Cart Total'
+                    },
+                    unit_amount: cartTotalPrice * 100
+                },
+                quantity: 1,
+            },
+        ],
         mode: 'payment',
         success_url: `${YOUR_DOMAIN}?success=true`,
         cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
-
+    console.log(session.url);
     res.redirect(303, session.url);
 });
+
+app.listen(3000, () => console.log('Running on port 3000'));
